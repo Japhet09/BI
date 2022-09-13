094081a5fb01fbf6c01113a0a264d4609a93520d
Japhet <japhetalfred19@gmail.com>
2022-09-13 13:08:36 +0200

    answered question 2 and 3

--- Python&R/lab1/r_lab_assignment.R
+++ Python&R/lab1/r_lab_assignment.R
@@ -1,2 +1,19 @@
 # Lab assignment
-# Basic R usage and data manipulation ---
+# Basic R usage and data manipulation ----
+#QUESTION 1 ----
+
+vec1 <- c(0, 2, 3, 0, 2, 11, 0, 7, NA)
+
+# a) Removing the NA value
+vec1_remove_na <- vec1[!is.na(vec1)]
+vec1_remove_na
+
+# b) logical vector where  0 == TRUE, otherwise FALSE
+vec1_logical <- vec1 == 0
+vec1_logical
+
+# c) nonzero values 
+
+# d) length of non zero values
+
+# QUESTION 2 ----
