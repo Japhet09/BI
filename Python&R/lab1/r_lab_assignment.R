c6d4340f1b7301e13bd9f32f68ec5b64f127b8fd
Japhet <japhetalfred19@gmail.com>
2022-09-14 10:38:43 +0200

    finished question 4 part e

--- Python&R/lab1/r_lab_assignment.R
+++ Python&R/lab1/r_lab_assignment.R
@@ -1,20 +1,23 @@
-# Lab assignment
-# Basic R usage and data manipulation ----
+# Lab 1 assignment ----
+# Basic R usage and data manipulation
 #QUESTION 1 ----
 
 vec1 <- c(0, 2, 3, 0, 2, 11, 0, 7, NA)
 
 # a) Removing the NA value
-vec1_remove_na <- vec1[!is.na(vec1)]
-vec1_remove_na
+vec1 <- vec1[!is.na(vec1)]
+vec1
 
 # b) logical vector where  0 == TRUE, otherwise FALSE
 vec1_logical <- vec1 == 0
 vec1_logical
 
-# c) nonzero values 
+# c) nonzero values
+non_zero <- vec1[!vec1_logical]
+non_zero
 
 # d) length of non zero values
+length(non_zero)
 
 # QUESTION 2 ----
 
@@ -28,17 +31,31 @@ question2 <- data.frame(W, GENDER, YEAR)
 question2
 
 # b) exporting the dataframe as a csv
-write.table(question2, "/Users/mac/Desktop/BI/Python&R/lab1/question2.csv", sep =",", row.names = FALSE)
+write.table(
+  question2,
+  "/Users/mac/Desktop/BI/Python&R/lab1/question2.csv",
+  sep = ",",
+  dec = ".",
+  row.names = FALSE,
+  col.names = TRUE
+)
 
 # QUESTION 3 ----
 # a) Importing the data
-freedman <- read.table("/Users/mac/Desktop/BI/Python&R/lab1/Freedman.csv", sep = ",", dec = ".", header = TRUE, na.strings = NA)
+freedman <-
+  read.table(
+    "/Users/mac/Desktop/BI/Python&R/lab1/Freedman.csv",
+    sep = ",",
+    dec = ".",
+    header = TRUE,
+    na.strings = NA
+  )
 
 # b)
 summary(freedman)
 str(freedman)
 
-# c) 
+# c)
 # change the variables  to numeric
 freedman$population <- as.numeric(freedman$population)
 freedman$nonwhite <- as.numeric(freedman$nonwhite)
@@ -59,23 +76,34 @@ mean_crime <- mean(freedman$crime, na.rm = TRUE)
 
 # e) non white larger than 30%
 more_than30 <- freedman$nonwhite > 30
-freedman[more_than30, ]
+freedman[more_than30,]
 
 # QUESTION 4 ----
 # a) installing the Prestige package
 install.packages("car")
 library(car)
 
-# b)
-sub_prestige_women <- Prestige[Prestige$women > 50, ]
+# b) dataframe of occupations with more than 50% women
+sub_prestige_women <- Prestige[Prestige$women > 50,]
 
 # c) average prestige score
 mean(sub_prestige_women$prestige, na.rm = TRUE)
-# d) average prestige score women < 50%
+
+# d) average prestige score women less 50%
+below_50_women <- Prestige[Prestige$women < 50,]
+mean(below_50_women$prestige, na.rm = TRUE)
 
 # e)for loop
-for (i in 2:ncol(Prestige)) {
-  mean(Prestige$i, na.rm = TRUE)
-  
-}
+# initialize empty vector to store the mean
+occu_type_mean <- c()
 
+# store all unique occupation in a vector dropping missing values
+unique_occupation <- unique(Prestige$type)
+unique_occupation <- unique_occupation[!is.na(unique_occupation)]
+
+# loop through the unique occupations type
+for (i in unique_occupation) {
+  x <- mean(Prestige[Prestige$type == i,]$prestige, na.rm = TRUE)
+  occu_type_mean <- c(occu_type_mean, x)
+}
+occu_type_mean
