f1e9e80296fb6e9fdcbae48e605843053a95cce1
Japhet <japhetalfred19@gmail.com>
2022-09-20 22:51:46 +0200

    added solution for queston 4

--- Python&R/lab2/lab2_assignment.py
+++ Python&R/lab2/lab2_assignment.py
@@ -75,6 +75,26 @@ def propert_taxes():
     
     print(f"Assessment value : ${assessment_value}.  Propert Tax: ${property_tax}")
     
+# Exercise 4: Sum of Numbers 
+# initialize a variable to store the the sum of numbers
+sum_nums = 0
+# Loop continues as long as a positive number is entered
+while True:
+    num = float(input("Number : "))
+    # add the number to the sum if its positive
+    if num >= 0:
+        sum_nums += num
+    else: # if its negative, display the sum and break out of the loop 
+        print(f"The sum of numbers is {sum_nums}")
+        break
+    
+# Exercise 5: Maximum of Two values
+def my_max(num1, num2):
+    if num1 < num2:
+        return num2
+    else:
+        return num1
+        
     
     
     
