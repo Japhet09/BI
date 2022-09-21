07c6df8cb4a003d9e59a1924d05bdf7b8fffd7c9
Japhet <japhetalfred19@gmail.com>
2022-09-21 13:24:14 +0200

    created a constant for percentage,refactoring

--- Python&R/lab2/exercise3.py
+++ Python&R/lab2/exercise3.py
@@ -1,19 +1,27 @@
 # Exercise 3: Property Tax
-# Calculate the property tax based on the assessment value
-def propert_taxes():
-    # The percentage to get the assessment value from the actual value
-    percent_actual_value = 0.60
+# A constant percentage of the actual value  to get the assessment value
+PERCENT = 0.60
 
+def main():
     # Ask user for the actual property  value and convert it to float
     actual_value = float(input("Actual Property Value: "))
+    property_taxes(actual_value)
 
-    # Calculate the assessment value from the actual property value
-    assessment_value = actual_value * percent_actual_value
 
-    # to get the tax per dollar divide $0.72 (72¢) by $100
+# Calculate the property tax based on the assessment value
+def property_taxes(actual_value):
+    # Calculate the assessment value as the 60% of the actual property value
+    assessment_value = actual_value * PERCENT
+
+    # To get the tax per dollar divide $0.72 (72¢) by $100
     tax_per_dollar = 0.72 / 100
 
     # Calculate the property tax
     property_tax = assessment_value * tax_per_dollar
+    # Display the assessment value and  property tax rounded to 2 decimal places
+    print(
+        f"Assessment value: ${assessment_value:.2f}  Propert Tax: ${property_tax:.2f}"
+    )
 
-    print(f"Assessment value : ${assessment_value}.  Propert Tax: ${property_tax}")
+
+main()
