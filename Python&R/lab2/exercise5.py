0e089cda70e0d141f5ea292a3572bfc85577a054
Japhet <japhetalfred19@gmail.com>
2022-09-21 12:25:28 +0200

    added a wrapper function and changed user input from float to int

--- Python&R/lab2/exercise5.py
+++ Python&R/lab2/exercise5.py
@@ -1,6 +1,11 @@
 # Exercise 5: Maximum of Two values
-# Function accepts two float parameters and returns the maximum value
+def main():
+    # Convert user input to integer values
+    num1 = int(input("Enter num1: "))
+    num2 = int(input("Enter num2: "))
+    print(f"The greater value is {my_max(num1, num2)}")
 
+# Function accepts two float parameters and returns the maximum value
 
 def my_max(num1, num2):
     # If num1 is smaller than num2, then return num2
@@ -9,3 +14,5 @@ def my_max(num1, num2):
     # if the conditon above is False, that means num1 is greater than num2
     else:
         return num1
+
+main()
