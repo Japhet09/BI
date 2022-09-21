eaf3b8470ca315f4e4a9bf4b391bea356571f69c
Japhet <japhetalfred19@gmail.com>
2022-09-21 13:30:51 +0200

    refactoring

--- Python&R/lab2/exercise5.py
+++ Python&R/lab2/exercise5.py
@@ -1,11 +1,14 @@
 # Exercise 5: Maximum of Two values
+# Create a wrapper function
 def main():
     # Convert user input to integer values
     num1 = int(input("Enter num1: "))
     num2 = int(input("Enter num2: "))
     print(f"The greater value is {my_max(num1, num2)}")
 
+
 # Function accepts two float parameters and returns the maximum value
+
 
 def my_max(num1, num2):
     # If num1 is smaller than num2, then return num2
@@ -14,5 +17,6 @@ def my_max(num1, num2):
     # if the conditon above is False, that means num1 is greater than num2
     else:
         return num1
+
 
 main()
