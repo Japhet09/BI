7e7e40174a89407aef9683f00ee3f4dae7ae3c7e
Japhet <japhetalfred19@gmail.com>
2022-09-25 19:09:32 +0200

    change comparison operator to include edge cases (float value)

--- Python&R/lab2/exercise6.py
+++ Python&R/lab2/exercise6.py
@@ -1,6 +1,6 @@
 # Exercise 6: Test Average and Grade
 def main():
-    score = int(input("Score: "))
+    score = float(input("Score: "))
     print(f"You scored {determine_grade(score)}")
 
 
@@ -14,13 +14,13 @@ def calc_average(score1, score2, score3, score4, score5):
 def determine_grade(score):
     if 90 <= score <= 100:
         return "A"
-    elif 80 <= score <= 89:
+    elif 80 <= score < 90:
         return "B"
-    elif 70 <= score <= 79:
+    elif 70 <= score < 80:
         return "C"
-    elif 60 <= score <= 69:
+    elif 60 <= score < 70:
         return "D"
-    elif score < 60:
+    elif score < 60: # used an elif instead of else to only return score below 60 and not 
         return "F"
 
 
