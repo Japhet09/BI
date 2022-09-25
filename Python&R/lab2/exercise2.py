b4900f1a1d46a6b0d0451cae06c201dfac0423da
Japhet <japhetalfred19@gmail.com>
2022-09-25 19:11:46 +0200

    renaming a variable

--- Python&R/lab2/exercise2.py
+++ Python&R/lab2/exercise2.py
@@ -6,14 +6,14 @@ weight = float(input("Enter weight in kg: "))
 height = float(input("Enter height in meter: "))
 
 # Calculate the user bmi_index and round to 2 decimal places
-bmi_index = round((weight / (height**2)), 2)
+bm_index = round((weight / (height**2)), 2)
 
 # Then display their bmi with a message depending on their bmi_index
-if bmi_index < 18.5:
-    print(f"You are Underweight with a  bmi index of {bmi_index}")
-elif 18.5 <= bmi_index <= 24.99:
-    print(f"Your weight is normal with a bmi index of {bmi_index}")
-elif 25 <= bmi_index <= 29.99:
-    print(f"You are overweight with a bmi index of {bmi_index}")
-elif bmi_index >= 30:
-    print(f"You have obesity with a bmi index of {bmi_index}")
+if bm_index < 18.5:
+    print(f"You are Underweight with a  bmi index of {bm_index}")
+elif 18.5 <= bm_index <= 24.99:
+    print(f"Your weight is normal with a bmi index of {bm_index}")
+elif 25.0 <= bm_index <= 29.99:
+    print(f"You are overweight with a bmi index of {bm_index}")
+elif bm_index >= 30:
+    print(f"You have obesity with a bmi index of {bm_index}")
