aa6f79b47551b5bf7feb2d54be2bdcb1001d07b9
Japhet <japhetalfred19@gmail.com>
2022-10-03 12:33:55 +0200

    created a class with its setter and getter methods

---
+++ Python&R/lab3/pet.py
@@ -0,0 +1,28 @@
+# Class name
+class Pet:
+    # initialize the class constructor
+    def __init__(self, name, animal_type, age):
+        self.__name = name
+        self.__animal_type = animal_type
+        self.__age = age
+        
+    # methods to change the class attributes
+    def set_name(self, name):
+        self.__name = name
+        
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+    
+    def set_age(self, age):
+        self.__age = age
+        
+    # methods to access the class attributes
+    def get_age(self):
+        return self.__age
+    
+    def get_name(self):
+        return self.__name
+    
+    def get_animal_type(self):
+        return self.__animal_type
+        
