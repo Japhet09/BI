b94c9db1c67135b4da6b1e256645de7ee63489e7
Japhet <japhetalfred19@gmail.com>
2022-10-05 10:11:35 +0200

    scores to be entered by the user

--- Python&R/lab2/exercise6.py
+++ Python&R/lab2/exercise6.py
@@ -1,7 +1,24 @@
 # Exercise 6: Test Average and Grade
 def main():
-    score = float(input("Score: "))
-    print(f"You scored {determine_grade(score)}")
+    # Ask the user to enter the scores and display the grade of that score.
+    score1 = float(input("Score: "))
+    print(f"Grade {determine_grade(score1)}")
+
+    score2 = float(input("Score: "))
+    print(f"Grade {determine_grade(score2)}")
+
+    score3 = float(input("Score: "))
+    print(f"Grade {determine_grade(score3)}")
+
+    score4 = float(input("Score: "))
+    print(f"Grade  {determine_grade(score4)}")
+
+    score5 = float(input("Score: "))
+    print(f"Grade {determine_grade(score5)}")
+
+    average_score = calc_average(score1, score2, score3, score4, score5)
+    print(f"Average score: {average_score}")
+    print(f"Grade {determine_grade(average_score)}")
 
 
 # Function that calcualte and returns the average of 5 score
@@ -20,7 +37,9 @@ def determine_grade(score):
         return "C"
     elif 60 <= score < 70:
         return "D"
-    elif score < 60: # used an elif instead of else to only return score below 60 and not 
+    elif (
+        score < 60
+    ):  # used an elif instead of else to only return score below 60 and not
         return "F"
 
 
