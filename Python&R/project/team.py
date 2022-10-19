f1dad0fd59dd815b9f50f7eb5cadb8983b98f278
Japhet <japhetalfred19@gmail.com>
2022-10-19 10:12:27 +0200

    removed decorators and used getters and setter

--- Python&R/project/team.py
+++ Python&R/project/team.py
@@ -25,102 +25,103 @@ class Team:
 
     # accessor methods for  the attributes (getter)
 
-    # Read only method (will not have a correspomding mutator method/setter)
-    @property
-    def id(self):
+    # Read only method for the id(will not have a correspomding mutator method/setter)
+
+    def get_id(self):
         """Return the id of the team"""
         print("getting id")
         return self.__id
 
-    # Read only method (will not have a correspomding mutator method/setter)
-    @property
-    def date(self):
+    # Read only method for the date(will not have a correspomding mutator method/setter)
+
+    def get_date(self):
         """Return the date the team was created"""
         print("getting date")
         return self.__date
 
-    @property
-    def name(self):
+    # accessor method for getting the name of the team
+    def get_name(self):
         """Return the name of the team"""
         print("getting name")
-        return self._name
+        return self.name
 
-    @name.setter
-    def name(self, name):
+    # mutator method for setting the name of the team
+    def set_name(self, name):
         """Set the name of the team"""
         print("setting name")
 
-        # Check if name is an instance of class string
         # if name is not a string, raise a Value error
 
         if type(name) == str:
-            self._name = name
+            self.name = name.upper()
         else:
             raise ValueError("Name must be a string")
 
-    @property
-    def type(self):
+    # accessor method for getting the type of the team
+    def get_type(self):
         """Return the type of the team"""
         print("getting types")
-        return self._type
+        return self.type
 
-    @type.setter
-    def type(self, type):
+    # Mutator method for setting the type of the team
+    def set_type(self, type):
         """Set the type of the team"""
         print("setting type")
         # team type can only be boys or girls
-        if type.lower() in ["boys", "girls"]:
-            self._type = type
+        if type.upper() in ["BOYS", "GIRLS"]:
+            self.type = type.upper()
         else:
             raise ValueError("Invalid team type: can only be boys/girls")
 
-    @property
-    def fee_paid(self):
+    # accessor method for getting the fee status
+    def get_fee_paid(self):
         """Return the status of the fee if paid or not"""
         print("getting fee paid")
-        return self._fee_paid
+        return self.fee_paid
 
-    @fee_paid.setter
-    def fee_paid(self, fee_paid):
+    # mutator method for setting the fee status
+    def set_fee_paid(self, fee_paid):
         """Set the status of the fee if paid or not"""
         print("setting fee paid")
         # chekc if fee_paid is of class boolean
         # if fee_paid is not of class boolean, then raise a value error
         if type(fee_paid) == bool:
-            self._fee_paid = fee_paid
+            self.fee_paid = fee_paid
         else:
             raise ValueError("fee_paid must be a boolean True/False")
 
-    @property
-    def fee_amount(self):
+    # accessor method for getting the fee amount
+    def get_fee_amount(self):
         """Returns the amount of the fee paid"""
         print("getting fee_amount")
-        return self._fee_amount
+        return self.fee_amount
 
-    @fee_amount.setter
-    def fee_amount(self, fee_amount):
+    # mutator method for setting the fee amount
+    def set_fee_amount(self, fee_amount):
         """Set the amount of the fee paid"""
         print("setting fee_amount")
+        # if fee_amount is not an integer raise ValueError
         if type(fee_amount) == int:
-            self._fee_amount = fee_amount
+            self.fee_amount = fee_amount
         else:
             raise ValueError("fee_amount must be an integer")
-
+        
+    # A method to to store the team information as a dictionary
     def store_data(self):
         return {
             "id": self.__id,
             "date": self.__date,
-            "name": self._name,
-            "type": self._type,
-            "fee": self._fee_paid,
-            "fee_amount": self._fee_amount,
+            "name": self.name,
+            "type": self.type,
+            "fee": self.fee_paid,
+            "fee_amount": self.fee_amount,
         }
-
+        
+    # A method to show the string representation of the team object
     def __str__(self):
-        n = "\n"
-        return f"ID: {self.__id}, Date: {self.__date}, Name: {self._name}, Type: {self._type}, Fee Paid: {self._fee_paid}, Fee Amount: {self._fee_amount}"
+        return f"ID: {self.__id}, Date: {self.__date}, Name: {self.name}, Type: {self.type}, Fee Paid: {self.fee_paid}, Fee Amount: {self.fee_amount}"
 
-
+''' 
 t = Team("simba", "Boys", True, 99)
 t2 = Team("quenss", "girls", False, 0)
 t3 = Team("luton", "Boys", True, 990)
@@ -135,3 +136,4 @@ for t in lst:
     c.append(t.store_data())
 print(c)
 
+'''
