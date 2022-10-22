55c69bacd2be1e2cb1cf69681afe93f79f34327d
Japhet <japhetalfred19@gmail.com>
2022-10-22 11:45:54 +0200

    chages from cs50 workspace

--- Python&R/project/team.py
+++ Python&R/project/team.py
@@ -10,99 +10,83 @@ class Team:
     # A class attribute that will be used to increment the id attribute for each object created
     counter = 1
 
-    def __init__(self, name, type, fee_paid):
+    def __init__(self, name, type, fee_paid, fee_amount = 99):
         """
         Initialize the team object
         """
         # use the class counter variable to initialize the id attribute
         self.__id = Team.counter  # private attribute
         Team.counter += 1  # increment the class variable for the next object creation
-
-        # get the current time and  then store the date as a string
-        now = datetime.datetime.now()
-        self.__date = now.strftime("%Y/%m/%d")  # private attribute
+        self.__date = datetime.date.today()  # private attribute
         self.name = name
         self.type = type
         self.fee_paid = fee_paid
-        self.fee_amount = 99  # default fee amount
+        self.fee_amount = fee_amount
 
-    # Read only method for the id (will not have a corresponding mutator method since it is private)
+    # Read only method for the id
     @property
     def id(self):
         """Return the id of the team"""
-        print("getting id")
         return self.__id
 
-    # Read only method for the date(will not have a corresponding mutator method since it is private)
+    # Read only method for the date
     @property
     def date(self):
         """Return the date the team was created"""
-        print("getting date")
         return self.__date
 
     # accessor method for getting the name of the team
     @property
     def name(self):
         """Return the name of the team"""
-        print("getting name")
         return self._name
 
     # mutator method for setting the name of the team
     @name.setter
     def name(self, name):
         """Set the name of the team"""
-        print("setting name")
-
-        # if name is not a string, raise a Value error
-        if type(name) != str:
-            raise ValueError("Name must be a string")
-        self._name = name.upper()
+        self._name = name.title()
 
     # accessor method for getting the type of the team
     @property
     def type(self):
         """Return the type of the team"""
-        print("getting types")
         return self._type
 
     # Mutator method for setting the type of the team
     @type.setter
     def type(self, type):
         """Set the type of the team"""
-        print("setting type")
         # team type can only be boys or girls
-        if type.upper() == "B":
-            self._type = "BOYS"
-        elif type.upper() == "G":
-            self._type = "GIRLS"
+        if type.lower() in ['b', 'boys','boy']:
+            self._type = "Boys"
+        elif type.lower() in ['g','girls','girl']:
+            self._type = "Girls"
         else:
-            raise ValueError("Invalid team type: Enter B or G")
+            raise ValueError("Invalid team type ")
 
     # accessor method for getting the fee status
     @property
     def fee_paid(self):
         """Return the status of the fee if paid or not"""
-        print("getting fee paid")
         return self._fee_paid
 
     # mutator method for setting the fee status
     @fee_paid.setter
     def fee_paid(self, fee_paid):
         """Set the status of the fee if paid or not"""
-        print("setting fee paid")
         # Fee is paid True(Y) or False (N)
-        if fee_paid.upper() == "Y":
+        if fee_paid.lower() in ['y', 'yes', 'true']:
             self._fee_paid = True
-        elif fee_paid.upper() == "N":
+        elif fee_paid.lower() in ['n', 'no', 'false']:
             self._fee_paid = False
         else:
-            raise ValueError("Enter Y or N")
+            raise ValueError("Enter Yes or No")
 
     # accessor method for getting the fee amount
     @property
     def fee_amount(self):
         """Returns the amount of the fee paid"""
-        print("getting fee_amount")
         return self._fee_amount
 
     # mutator method for setting the fee amount
@@ -112,39 +96,26 @@ class Team:
         Set the amount of the fee paid
         depending on the fee status
         """
-        print("setting fee_amount")
         # If fee is paid, set fee_amount to default value(99)
         if self._fee_paid == True:
             self._fee_amount = fee_amount
         else:
             self._fee_amount = 0
 
-    # A method to to store the team information as a dictionary
-    def store_data(self):
-        """Return a dictionary of team information"""
-        return {
-            "id": self.__id,
-            "date": self.__date,
-            "name": self._name,
-            "type": self._type,
-            "fee_paid": self._fee_paid,
-            "fee_amount": self._fee_amount,
-        }
     def __str__(self):
-        return f"ID: {self.__id}, Date: {self.__date}, Name: {self._name}, Type: {self._type}, Fee Paid: {self._fee_paid}, Fee Amount: {self._fee_amount}"
-
+        return (f"Team Info\n"
+        f"ID: {self.__id}\n"
+        f"Date: {self.__date}\n"
+        f"Name: {self._name}\n"
+        f"Type: {self._type}\n"
+        f"Fee Paid: {self._fee_paid}\n"
+        f"Fee Amount: {self._fee_amount}\n")
 '''
-t = Team("simba", "B", "Y")
-t2 = Team("quenss", "g", "Y")
-t3 = Team("luton", "B", "Y")
-t4 = Team("jkt", "B", "N")
-t5 = Team("atleti", "B", "N")
-t6 = Team("crown", "g", "Y")
+t = Team('Simba', 'B', 'Y')
+t2 = Team('Young', 'g', 'n')
 
-lst = [t, t2, t3, t4, t5, t6]
-c = []
+#t.update_type('yeye')
+#print(t,t2)
 
-for t in lst:
-    c.append(t.store_data())
-print(c)
+print(t.date)
 '''
