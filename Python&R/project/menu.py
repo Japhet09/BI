7b844c9d3dfccfb4813c2be65825692bd3d2db11
Japhet <japhetalfred19@gmail.com>
2022-10-31 12:53:48 +0100

    rearranging the conditional statements

--- Python&R/project/menu.py
+++ Python&R/project/menu.py
@@ -1,6 +1,7 @@
-# import menu to be able to use all
-# the functions in the menu script
-# import sys to use it to exit the program
+# This program import the Interface class from the interface module
+# and perform differrent operations according to the user's choice.
+# To quit the program, sys module is used
+# =======================================================================
 import sys
 from interface import Interface
 
@@ -17,19 +18,19 @@ TOTAL_TEAMS = 9
 CANCEL_PARTICIPATION = 10
 QUIT_CHOICE = 0
 
-# constants for the SUB MENU FOR UPDATE_TEAMS
+# constants for the sub-menu to update teams
 UPDATE_NAME = 51
 UPDATE_TYPE = 52
 UPDATE_FEES = 53
 
 
 def main():
-
+    # Initialize an infinite loop, exit when the user enter 0
     while True:
-        # DISPLAY THE MENU
+        # DISPLAY THE MAIN MENU
         Interface.display_main_menu()
         try:
-            # get the user choice
+            # Get the user choice
             choice = int(input("Enter choice from main menu: "))
             if choice == CREATE:
                 Interface.create_team()
@@ -39,16 +40,6 @@ def main():
                 Interface.show_team_by_type()
             elif choice == SHOW_ALL_TEAMS:
                 Interface.show_all_teams()
-            elif choice == QUIT_CHOICE:
-                sys.exit("Exiting the program")
-            elif choice == SAVE_DATA:
-                Interface.save_data()
-            elif choice == READ_DATA:
-                Interface.read_data()
-            elif choice == DELETE_TEAM:
-                Interface.delete_team()
-            elif choice == TOTAL_TEAMS:
-                Interface.count_teams()
             elif choice == CANCEL_PARTICIPATION:
                 Interface.participation()
             elif choice == UPDATE_TEAMS:
@@ -63,13 +54,28 @@ def main():
                     Interface.update_fees()
                 else:
                     print("You can only update team name/type/fee_status")
+            elif choice == DELETE_TEAM:
+                Interface.delete_team()
+            elif choice == SAVE_DATA:
+                Interface.save_data()
+            elif choice == READ_DATA:
+                Interface.read_data()
+            elif choice == TOTAL_TEAMS:
+                Interface.count_teams()
+            elif choice == QUIT_CHOICE:
+                sys.exit("Exiting the program")
             else:
                 print("Error: Invalid choice, use the menu")
                 continue
         except ValueError:
             print("Choice should be an integer")
             continue
+        except FileNotFoundError:
+            # This happens when the user read_data before saving it to disk
+            print("Make sure to save the teams before restoring the file")
+            continue
 
 
+# Call the main function
 if __name__ == "__main__":
     main()
